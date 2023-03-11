b6abf64c17fe12696f542384cc069580a4819319
Lydia <lydia.cupery@atomicobject.com>
2023-03-10 20:42:05 -0500

    add back in the worker

--- client/src/Resume.tsx
+++ client/src/Resume.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useRef, useEffect, useCallback } from "react";
-import { Document, Page, pdfjs } from "react-pdf/dist/esm/entry.webpack5";
+import {
+  Document,
+  Page,
+  pdfjs,
+  PDFDownloadLink,
+} from "react-pdf/dist/esm/entry.webpack5";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
@@ -24,6 +29,23 @@ export const Resume: React.FC<{}> = () => {
   //     img.removeEventListener("load", onLoad);
   //   };
   // }, [onLoad]);
+  const Resume = (
+    <Document
+      options={{
+        workerSrc: "pdf.worker.js",
+      }}
+      // file={
+      //   "https://drive.google.com/file/d/1oCFPb6SYnweyNR9lk2QcewJvRs9PnczX/view?usp=sharing"
+      // }
+      // file={{
+      //   url: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK",
+      // }}
+      file={pdf}
+      onLoadError={console.error}
+    >
+      <Page pageNumber={1} />
+    </Document>
+  );
   return (
     <div style={{ marginTop: "20px", height: "100%" }}>
       {/* <img
@@ -43,21 +65,12 @@ export const Resume: React.FC<{}> = () => {
         width="1100"
         title="Resume"
       /> */}
-      <Document
-        options={{
-          workerSrc: "pdf.worker.js",
-        }}
-        // file={
-        //   "https://drive.google.com/file/d/1oCFPb6SYnweyNR9lk2QcewJvRs9PnczX/view?usp=sharing"
-        // }
-        // file={{
-        //   url: "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK",
-        // }}
-        file={pdf}
-        onLoadError={console.error}
-      >
-        <Page pageNumber={1} />
-      </Document>
+      {Resume}
+      <PDFDownloadLink document={Resume} fileName="resume.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? "Loading document..." : "Download now!"
+        }
+      </PDFDownloadLink>
     </div>
   );
 };

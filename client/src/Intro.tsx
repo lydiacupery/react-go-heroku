44109907136628c64aae19809a0c541ffd315de9
Lydia <lydia.cupery@atomicobject.com>
2023-03-31 09:24:43 -0400

    continue updating intro

--- client/src/Intro.tsx
+++ client/src/Intro.tsx
@@ -42,17 +42,18 @@ export const Intro: React.FC<{}> = (props) => {
         component parts.
       </p>
       <p style={{ width: "50%", textAlign: "left" }}>
-        I started working at Atomic, a consultancy in Grand Rapids, right after
-        graduation. I'm a full stack developer At Atomic. At Atomic, we use a
-        wide range of technologies to build custom products for our clients.
-        Some technologies I'm very familiar with AWS, PSQL, Node, Typescript,
-        Docker, React, and CSS. You can find some of my blog posts about custom
+        Soon after graduation, I began my career as a full stack developer at
+        Atomic, a Grand Rapids-based consultancy where we employ a variety of
+        technologies to create customized products for our clients. I have a lot
+        of experience with AWS, PSQL, Node, Typescript, Docker, React, and CSS.
+        You can find some of my blog posts about those technologies and custom
         software development{" "}
         <a href="https://spin.atomicobject.com/author/lydia-cupery/">here</a>.
       </p>
       <p style={{ width: "50%", textAlign: "left" }}>
-        I really enjoy understanding how all parts of a system fit together -
-        from database constraints to frontend animations.
+        What I find most fulfilling about my work is gaining a deep
+        understanding of how all the moving parts of a system work together,
+        from backend constraints to frontend animations.
       </p>
     </>
   );

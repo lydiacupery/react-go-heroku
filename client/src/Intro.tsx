b6f5d06999b883aadd1f51e6cc38feb9d9f8ce69
Lydia <lydia.cupery@atomicobject.com>
2023-10-09 19:40:09 -0400

    update alignment of text

--- client/src/Intro.tsx
+++ client/src/Intro.tsx
@@ -68,15 +68,14 @@ export const Intro: React.FC<{}> = (props) => {
         software development{" "}
         <a href="https://spin.atomicobject.com/author/lydia-cupery/">here</a>.
       </p>
-      <p>
+      <p style={{ textAlign: "left" }}>
         In 2023, I joined HubSpot as a Senior Software Engineer. I'm a frontend
         developer on the documentation & onboarding team where we build tools
         for HubSpot developers.
       </p>
       <p style={{ textAlign: "left" }}>
-        What I find most fulfilling about my work is gaining a deep
-        understanding of how all the moving parts of a system work together,
-        from backend constraints to frontend animations.
+        What I find most fulfilling about my work is understanding of how all
+        the moving parts of a system work together.
       </p>
     </div>
   );
